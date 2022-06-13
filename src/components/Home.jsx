74501a33c7f3ac040331234ea6a1d2e23813f487
mikiimikii <hajorge2012@gmail.com>
2022-06-13 00:55:17 -0300

    edit text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,12 +39,11 @@ const Home = () => {
             Im a full stack developer
           </h2>
           <p className="text-[#8892b0] py-4 max-w[700px]">
-            I’m a full-stack developer specializing in building (and
+            I’m a developer specializing in building (and
             occasionally designing) exceptional digital experiences. Currently,
-            I’m focused on building responsive full-stack web applications.
+            I’m focused on building responsive web applications.
           </p>
         </motion.div>
-
         <div className="z-40">
           <Link onClick={handleClick} to="work" smooth={true} duration={500}>
             <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
